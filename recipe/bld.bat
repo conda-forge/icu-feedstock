56753842720ea96796524a725d471ce8b757e85b
Michael Sarahan <msarahan@gmail.com>
2016-03-27 21:48:46 -0500

    windows build with msys2

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,15 +1,29 @@
-if "%ARCH%"=="32" (
-   set ARCH=Win32
-   set COPYSUFFIX=
-) else (
-  set ARCH=x64
-  set COPYSUFFIX=64
-)
+cd source
 
-msbuild source\allinone\allinone.sln /p:Configuration=Release;Platform=%ARCH%
+:: MSYS includes a link.exe that is not MSVC's linker.  Hide it.
+MOVE %LIBRARY_PREFIX%\usr\bin\link.exe %LIBRARY_PREFIX%\usr\bin\link.exe.backup
 
-ROBOCOPY bin%COPYSUFFIX% %LIBRARY_BIN% *.dll /E
-ROBOCOPY lib%COPYSUFFIX% %LIBRARY_LIB% *.lib /E
-ROBOCOPY include %LIBRARY_inc% * /E
+set LIBRARY_PREFIX=%LIBRARY_PREFIX:\=/%
+set SRC_DIR=%SRC_DIR:\=/%
+set MSYSTEM=MINGW%ARCH%
+set MSYS2_PATH_TYPE=inherit
+set CHERE_INVOKING=1
+:: Set flag saying we have stdint.h manually.  We take care of providing it where
+::    it is needed, but missing, by using msinttypes
+set "CXXFLAGS=%CXXFLAGS% -DU_HAVE_STDINT_H=1"
+set "CFLAGS=%CFLAGS% -DU_HAVE_STDINT_H=1"
 
-if %ERRORLEVEL% LSS 8 exit 0
+bash runConfigureICU MSYS/MSVC --prefix=%LIBRARY_PREFIX% --enable-static
+if errorlevel 1 exit 1
+make
+if errorlevel 1 exit 1
+make check
+if errorlevel 1 exit 1
+make install
+if errorlevel 1 exit 1
+
+set LIBRARY_PREFIX=%LIBRARY_PREFIX:/=\%
+
+:: Restore MSYS' link.exe
+MOVE %LIBRARY_PREFIX%\usr\bin\link.exe.backup %LIBRARY_PREFIX%\usr\bin\link.exe
+exit 0
