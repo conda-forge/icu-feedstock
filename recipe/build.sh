4d377bc59d0a8fd249f887d74b8eccba19e9ecc8
ocefpaf <ocefpaf@gmail.com>
2016-07-25 13:45:05 -0300

    rerender

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,33 +1,28 @@
 #!/bin/bash
 
-# FIXME: This is a hack to make sure the environment is activated.
-# The reason this is required is due to the conda-build issue
-# mentioned below.
-#
-# https://github.com/conda/conda-build/issues/910
-#
-source activate "${CONDA_DEFAULT_ENV}"
-
 cd source
 chmod +x configure install-sh
 
 EXTRA_OPTS=""
-if [ "$(uname)" == "Darwin" ];
-then
-    EXTRA_OPTS="--enable-rpath"
-    export CXX="${CXX} -stdlib=libc++"
+
+if [ $(uname) == Darwin ]; then
+  EXTRA_OPTS="--enable-rpath"
+  export CC=clang
+  export CXX=clang++
+  export MACOSX_DEPLOYMENT_TARGET="10.9"
+  export CXXFLAGS="-stdlib=libc++ $CXXFLAGS"
+  export CXXFLAGS="$CXXFLAGS -stdlib=libc++"
 fi
 
-./configure --prefix="$PREFIX" \
+
+./configure --prefix=$PREFIX \
             --disable-samples \
             --disable-extras \
             --disable-layout \
             --disable-tests \
             --enable-static \
-	    "${EXTRA_OPTS}"
+            "$EXTRA_OPTS"
 
-make -j$CPU_COUNT
+make
 make check
 make install
-
-rm -rf $PREFIX/sbin
