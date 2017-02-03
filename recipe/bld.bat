454382a6446d3f342a285392a9c57f300d70371c
Sam Gillingham <gillingham.sam@gmail.com>
2017-02-03 12:48:27 +1000

    Attempt to fix VS2008 build by copying a more recent msbuild like xerces-c

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,10 +6,19 @@ if "%ARCH%"=="32" (
   set COPYSUFFIX=64
 )
 
+:: Need to move a more current msbuild into PATH.  The one on AppVeyor barfs on the solution
+::     This one comes from the Win7 SDK (.net 4.0), and is known to work.
+if %VS_MAJOR% == 9 (
+    COPY C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe .\
+    set "PATH=%CD%;%PATH%"
+)
+
 msbuild source\allinone\allinone.sln /p:Configuration=Release;Platform=%ARCH%
+if errorlevel 1 exit 1
 
 ROBOCOPY bin%COPYSUFFIX% %LIBRARY_BIN% *.dll /E
+if %ERRORLEVEL% LSS 8 exit 0
 ROBOCOPY lib%COPYSUFFIX% %LIBRARY_LIB% *.lib /E
+if %ERRORLEVEL% LSS 8 exit 0
 ROBOCOPY include %LIBRARY_inc% * /E
-
 if %ERRORLEVEL% LSS 8 exit 0
