2df1eac9da9ae23cedf132974b04d30f64995cd0
Sam Gillingham <gillingham.sam@gmail.com>
2017-02-04 09:01:55 +1000

    skip py36 on Win and fix rc.exe/msys2 timing issue

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,16 +1,20 @@
 
 cd source
 
-:: I don't understand but running rc.exe fails without this...
+:: This seems to be required - not sure why but
+:: rc.exe gets confused with the '/' form of slashes
 set MSYS_RC_MODE=1
 
 :: Can't seem to determine msys2 due to bug in config.guess,
 :: BUT runConfigureICU expects cygwin, so we just pretend we are
-bash runConfigureICU Cygwin/MSVC --build=x86_64-pc-cygwin --prefix=${CYGWIN_PREFIX}/Library
+bash runConfigureICU Cygwin/MSVC --build=x86_64-pc-cygwin --prefix=%CYGWIN_PREFIX%/Library
 :: Ignore errorlevel - there are warnings about various things missing
 :: which we don't actually seem to need. Just keep going...
 ::if errorlevel 1 exit 1
 
+make -j%CPU_COUNT%
+:: Run make twice. There is some timing issue between msys2 and rc.exe
+:: that means that directories are created after they are required...
 make -j%CPU_COUNT%
 if errorlevel 1 exit 1
 
