5fcc32cf630d63c930e3981e2e54b27b9af16b32
Filipe Fernandes <ocefpaf@gmail.com>
2018-06-29 18:02:22 -0300

    update recipe

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,21 +5,30 @@ cd source
 chmod +x configure install-sh
 
 EXTRA_OPTS=""
-if [ "$(uname)" == "Darwin" ]; then
+if [[ ${HOST} =~ .*darwin.* ]]; then
   EXTRA_OPTS="--enable-rpath"
-  export CXX="${CXX} -stdlib=libc++"
+  # TODO :: Fix this libtool cross-compilation bug
+  export CC=$(dirname $(which ${CC}))/clang
+  export CXX=$(dirname $(which ${CXX}))/clang++
+elif [[ ${HOST} =~ .*linux.* ]]; then
+  # TODO :: This is a hack until we make it so that strong run-exports in requirements/build cause those
+  #         packages to be installed ino the host prefix during the build. This hack will not work for
+  #         real cross-compilation!
+  export LD_LIBRARY_PATH=${PREFIX}/${HOST}/lib:${LD_LIBRARY_PATH}
 fi
 
-./configure --prefix="$PREFIX" \
-            --disable-samples \
-            --disable-extras \
-            --disable-layout \
-            --disable-tests \
-            --enable-static \
+./configure --prefix="${PREFIX}"  \
+            --build=${BUILD}      \
+            --host=${HOST}        \
+            --disable-samples     \
+            --disable-extras      \
+            --disable-layout      \
+            --disable-tests       \
+            --enable-static       \
             "${EXTRA_OPTS}"
 
-make -j$CPU_COUNT
+make -j${CPU_COUNT} ${VERBOSE_CM}
 make check
 make install
 
-rm -rf $PREFIX/sbin
+rm -rf ${PREFIX}/sbin
