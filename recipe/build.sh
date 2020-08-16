d0e19e76b2f3d5a10d3754daefcc4a445c37e061
Isuru Fernando <isuruf@gmail.com>
2020-08-16 17:21:54 -0500

    Build for build machine first

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,16 +9,24 @@ cd source
 chmod +x configure install-sh
 
 EXTRA_OPTS=""
+
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
+    mkdir cross_build
+    pushd cross_build
+    CC=$CC_FOR_BUILD $CXX=$CXX_FOR_BUILD AR= AS= LD= CFLAGS= CXXFLAGS= LDFLAGS= CPPFLAGS= ./configure \
+      --build=${BUILD} \
+      --host=${BUILD} \
+      --disable-samples \
+      --disable-extras \
+      --disable-layout \
+      --disable-tests
+    make -j${CPU_COUNT}
+    popd
+    EXTRA_OPTS="$EXTRA_OPTS --with-cross-build=$SRC_DIR/cross_build"
+fi
+
 if [[ ${HOST} =~ .*darwin.* ]]; then
-  EXTRA_OPTS="--enable-rpath"
-  # TODO :: Fix this libtool cross-compilation bug
-  export CC=$(dirname $(which ${CC}))/clang
-  export CXX=$(dirname $(which ${CXX}))/clang++
-elif [[ ${HOST} =~ .*linux.* ]]; then
-  # TODO :: This is a hack until we make it so that strong run-exports in requirements/build cause those
-  #         packages to be installed ino the host prefix during the build. This hack will not work for
-  #         real cross-compilation!
-  export LD_LIBRARY_PATH=${PREFIX}/${HOST}/lib:${LD_LIBRARY_PATH}
+  EXTRA_OPTS="$EXTRA_OPTS --enable-rpath"
 fi
 
 ./configure --prefix="${PREFIX}"  \
@@ -32,7 +40,7 @@ fi
 
 make -j${CPU_COUNT} ${VERBOSE_CM}
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
-make check
+  make check
 fi
 make install
 
