93114b0f191e71d5005e614406706f3d1671fc89
Isuru Fernando <isuruf@gmail.com>
2025-06-23 05:17:56 +0530

    Revert "fix host and build"
    This reverts commit a21c1e9a16aeb0e09b460777591a7f091953173b.

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,17 +10,17 @@ if [[ "${target_platform}" == win-* ]]; then
   export CXXFLAGS="$CXXFLAGS /std:c++17"
   # Tell it we're building for MSVC
   cp source/config/mh-msys-msvc source/config/mh-unknown
-  # EXEXT is wrong for *-pc-windows-msvc
+  # EXEXT is wrong for *-pc--windows
   sed -i.bak 's/EXEEXT=""/EXEEXT=.exe/g'  source/configure
   if [[ "${build_platform}" == "win-64" ]]; then
-    export BUILD=x86_64-pc-windows-msvc
+    export BUILD=x86_64-pc-windows
   elif [[ "${build_platform}" == "win-arm64" ]]; then
-    export BUILD=aarch64-pc-windows-msvc
+    export BUILD=aarch64-pc-windows
   fi
   if [[ "${target_platform}" == "win-64" ]]; then
-    export HOST=x86_64-pc-windows-msvc
+    export HOST=x86_64-pc-windows
   elif [[ "${target_platform}" == "win-arm64" ]]; then
-    export HOST=aarch64-pc-windows-msvc
+    export HOST=aarch64-pc-windows
   fi
 fi
 
