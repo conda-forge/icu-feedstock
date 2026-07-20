8ce5f8f8f5a465f1aa421d0276cf00bc85b0b80d
Tobias Fischer <info@tobiasfischer.info>
2026-07-21 07:50:45 +1000

    Pass pkg-config libraries to MSVC linker

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -6,7 +6,7 @@ if errorlevel 1 exit 1
 for /f "usebackq tokens=*" %%a in (`pkg-config --cflags icu-uc`) do set "PC_CFLAGS=%%a"
 for /f "usebackq tokens=*" %%a in (`pkg-config --msvc-syntax --libs --static icu-uc`) do set "PC_LIBS=%%a"
 
-%CC% %CFLAGS% %LDFLAGS% %PC_CFLAGS% %PC_LIBS% "%RECIPE_DIR%\test.c"
+%CC% %CFLAGS% %PC_CFLAGS% "%RECIPE_DIR%\test.c" /link %LDFLAGS% %PC_LIBS%
 if errorlevel 1 exit 1
 
 test.exe
